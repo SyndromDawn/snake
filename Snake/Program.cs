3c53046e5b2a4ec14c8d8747be1f670bb8c4aab2
Mosolov Alexander <m0so1ov@hotmail.com>
2020-06-29 15:43:05 +0300

    Закончен урок 3

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,7 +10,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            //Draw( p1.x, p1.y, p1.sym );
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            // Draw(p2.x, p2.y, p2.sym);
+            p2.Draw();
+
+
+          /*  int x1 = 1;
             int y1 = 3;
             char sym1 = '*';
 
@@ -20,13 +35,15 @@ namespace Snake
             int y2 = 5;
             char sym2 = '#';
 
-            Draw(x2, y2, sym2);
+            Draw(x2, y2, sym2);*/
+
+            Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char sym)
+        /*static void Draw(int x, int y, char sym)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
-        }
+        }*/
     }
 }
